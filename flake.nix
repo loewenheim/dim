b2890f3a5b57723c265ad1f7b101ac6de8a3be9a
Sebastian Zivota <loewenheim@mailbox.org>
2021-10-20 12:12:25 +0200

    WIP flake.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,37 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     nixCargoIntegration = {
-       url = "github:yusdacra/nix-cargo-integration";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs { root = ./.; };
+  outputs = inputs:
+    inputs.nixCargoIntegration.lib.makeOutputs {
+      root = ./.;
+      buildPlatform = "crate2nix";
+      defaultOutputs = {
+        package = "dim";
+        app = "dim";
+      };
+      overrides.common = prev: {
+        pkgs = prev.pkgs;
+        
+        runtimeLibs = prev.runtimeLibs
+          ++ (with prev.pkgs; [ sqlite yarn npm ffmpeg ]);
+          
+        buildInputs = prev.buildInputs
+          ++ (with prev.pkgs; [ libssl libva libva2 libva-drm2 ]);
+          
+        nativeBuildInputs = prev.nativeBuildInputs ++ [ ];
+
+        env = prev.env;
+      };
+
+      overrides.build = common: prev: {
+        rootFeatures = prev.rootFeatures ++ [ "embed_ui" ];
+        release = false;
+        runTests = prev.runTests;
+      };
+    };
 }
